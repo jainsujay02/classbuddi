a7c9d068c97f23b258250553b15a26abef2e0d94
jainsujay02 <sujayjain2121@gmail.com>
2023-02-21 11:05:07 -0800

    Footer Component Added
    Index.html: Added Open Sans as a font style from Google Fonts
    App.js: called footer component for all pages
    Footer.js: Created footer component with styling and added built by information.
    package.json: imported styled components to add css in JS.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   );
 }
