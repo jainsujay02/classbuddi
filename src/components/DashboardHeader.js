eec52c2213d0a62d5e820da36c815df695ee6a5b
Kimberly <kimhnguyeno@g.ucla.edu>
2023-03-06 13:55:59 -0800

    Dashboard DashboardHeader.js: Created header with subtext for main Dashboard component to inherit.

---
+++ src/components/DashboardHeader.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  alignItems: center;
+  justifyContent:center;
+  fontFamily: "'Poppins', sans-serif";
+`
+const HeadStyle = {
+  fontWeight: '600',
+  fontSize: '48px',
+  color: '#333333',
+  marginLeft: '100px',
+}
+
+const SubStyle = {
+  fontSize: '16px',
+  color: '#8C8C8C',
+  marginLeft: '100px',
+}
+
+function DashboardHeader() {
+  return(
+    <Container>
+      <div>
+        <div style={HeadStyle}>Hi, Joe B.</div>
+        <div style={SubStyle}>Let's take a look at your classes and contacts.</div>
+     </div>
+    </Container>
+  );
+}
+export default DashboardHeader;
