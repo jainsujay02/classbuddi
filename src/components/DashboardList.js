fc36f497d66edf96e0f1a4dbf8a054b307619b6c
Kimberly <kimhnguyeno@g.ucla.edu>
2023-03-06 14:22:20 -0800

    Dashboard DashboardList.js: Inherited the DashboardCard components to create a list of student profiles.

---
+++ src/components/DashboardList.js
@@ -0,0 +1,51 @@
+import DashboardCard from "./DashboardCard";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+
+ const ClassHeaderStyle = {
+   fontWeight: '600',
+   marginBottom: '30px',
+   fontSize: '22px'
+ }
+
+/* THIS COMPONENT'S FUNCTION
+* This component will inherit the DashboardCard components and will display
+* the list of contacts that the user has for each class.
+* March 3: Currently, the code has static placeholder profiles
+* but it will be updated to handle and display the users in a list dynamically
+*/
+
+/*QUESTION
+* fixed scrolling function for lists?
+* how to dynamically pull list of students and class 
+*/
+
+function DashboardList(){
+  //will replace this first div with a function to handle 
+  //grabbing the header
+  return(
+    <div>
+      <div style={ClassHeaderStyle}>COM SCI 131</div>
+      <Box sx={{ 
+        width: 348,
+        height: 368,
+        display: 'flex',
+        }}> 
+        <Grid container rowSpacing={3}>
+          <Grid item>
+            <DashboardCard/>
+          </Grid>
+          <Grid item>
+            <DashboardCard/>
+          </Grid>
+          <Grid item>
+            <DashboardCard/>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
+
+export default DashboardList;
